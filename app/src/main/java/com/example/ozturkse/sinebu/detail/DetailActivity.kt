7a7bfa1b4a49d6eac788dff9a017da0b61ac9c77
Selim Öztürk <selim.ozturk@commencis.com>
2018-07-16 18:28:01 +0300

    add detail activity
    Change-Id: I6dd09db3db32151c704586368d6efdd08805cebf

---
+++ app/src/main/java/com/example/ozturkse/sinebu/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ozturkse.sinebu.detail
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
