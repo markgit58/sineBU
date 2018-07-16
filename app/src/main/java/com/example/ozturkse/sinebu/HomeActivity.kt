8ee1cdc3f853bb2279da09795dec91fbf67ee50d
Selim Öztürk <selim.ozturk@commencis.com>
2018-07-16 16:04:12 +0300

    add network call
    Change-Id: Ieae390c839d1fcfd453987d8c30ec1d408cfee7e

--- app/src/main/java/com/example/ozturkse/sinebu/HomeActivity.kt
+++ app/src/main/java/com/example/ozturkse/sinebu/HomeActivity.kt
@@ -2,11 +2,46 @@ package com.example.ozturkse.sinebu
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.Toast
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 class HomeActivity : AppCompatActivity() {
 
+    private val apikey = "65dd7f149cc5dc1f35fbedbc35c534ed"
+    private val baseUrl = "https://api.themoviedb.org/3/"
+
+    private val apiService by lazy {
+        TheMovieDbApiService.create(baseUrl)
+    }
+
+    private var disposable: Disposable? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        movies_list.layoutManager = LinearLayoutManager(this)
+
+        getMovies()
     }
+
+
+    fun getMovies() {
+        disposable = apiService.getTopRated(apiKey = apikey)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { result -> displayMovies(result.movies) },
+                        { error -> Toast.makeText(this, error.message, Toast.LENGTH_LONG).show() }
+                )
+
+    }
+
+    fun displayMovies(movies: List<Movie>?) {
+        movies_list.adapter = MyMovieRecyclerViewAdapter(movies)
+    }
+
+
 }
